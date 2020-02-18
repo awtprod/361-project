87c03855a0089daf7b07bb4d167f593d47b3242d
Andrew Ryan <ryanan@oregonstate.edu>
2020-02-18 17:48:27 +0000

    Consolidated database connection. Added user authentication functions.

--- server.js
+++ server.js
@@ -5,10 +5,45 @@ var bodyParser = require('body-parser');
 var mysql = require('./dbcon.js');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 const passport = require('passport');
+var Strategy = require('passport-local').Strategy;
 const path = require('path');
-const db = require('./models/db/db_config');
 
 const app = express();
+
+// Configure the local strategy for use by Passport.
+//
+// The local strategy require a `verify` function which receives the credentials
+// (`username` and `password`) submitted by the user.  The function must verify
+// that the password is correct and then invoke `cb` with a user object, which
+// will be set at `req.user` in route handlers after authentication.
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
+
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  The
+// typical implementation of this is as simple as supplying the user ID when
+// serializing, and querying the user record by ID from the database when
+// deserializing.
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
 
 
 app.set('view engine', 'hbs');
@@ -37,9 +72,24 @@ app.get('/login', (req, res) => {
     res.render('login.hbs');
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+  
+app.get('/logout',
+  function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
 
-});
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  });
+
 
 //This route handler is the "home page" of the server. It selects the resources
 //and products from the respective tables to send the home.handlebars
